f1fb1da945df43815c09d186ecd626919a868a23
Antonio Robres <twiindan@gmail.com>
2019-03-11 15:52:36 +0100

    Update send_sms_test.py

--- test/send_sms_test.py
+++ test/send_sms_test.py
@@ -5,7 +5,6 @@ def test_send_sms():
     app = TindevQA()
     user = 'Chewaka'
     text = 'Quieres enredarte entre mis pelos?'
-    to = 34600000000
     response = app.send_sms(user, text)
     assert response.ok == True
 
